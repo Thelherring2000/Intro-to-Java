3b4c653d3cb96209be520403a6d00d5bea4bb834
Greg <gregorybelcher6@gmail.com>
2017-12-01 13:17:43 -0600

    This my first test push.

---
+++ src/javaPrograms/AddSumOfTwoIntegers.java
@@ -0,0 +1,14 @@
+package javaPrograms;
+
+public class AddSumOfTwoIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int x= 8;
+int y= 4;
+int i;	
+
+System.out.println(i = x + y);
+}
+}
